dfb33c5aef16faf66fab385cc06c5912f5f9757d
Amanda Eliasson <amanda.eliasson@gmail.com>
2016-04-27 20:05:18 +0200

    Implemented AlarmActivity

--- app/src/main/java/com/axelfriberg/bikebuddy/NotificationFragment.java
+++ app/src/main/java/com/axelfriberg/bikebuddy/NotificationFragment.java
@@ -2,28 +2,39 @@ package com.axelfriberg.bikebuddy;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-
-public class NotificationFragment extends Fragment {
+public class NotificationFragment extends Fragment implements View.OnClickListener {
+    private Button b;
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getActivity().setTitle(R.string.navigation_notification);
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification,container,false);
-    }
 
+        View v =inflater.inflate(R.layout.fragment_notification,container,false);
+        b = (Button)v.findViewById(R.id.alarm_id);
+        b.setOnClickListener(this);
+        return v;
+    }
+    public void onClick(View v) {
+        Intent intent = new Intent(getActivity(), AlarmActivity.class);
+        startActivity(intent);
+    }
     @Override
     public void onResume(){
         super.onResume();
