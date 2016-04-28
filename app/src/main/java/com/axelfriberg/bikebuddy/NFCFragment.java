33a9b615014893790421c4ab2d3386ec82b60350
Axel Friberg <axl.friberg@gmail.com>
2016-04-28 14:28:48 +0200

    The map fragment is now apart of the MainActivity.

--- app/src/main/java/com/axelfriberg/bikebuddy/NFCFragment.java
+++ app/src/main/java/com/axelfriberg/bikebuddy/NFCFragment.java
@@ -1,7 +1,8 @@
 package com.axelfriberg.bikebuddy;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
- * Created by therese on 2016-04-25.
+ * Scans and NFC tag.
  */
 public class NFCFragment extends Fragment implements View.OnClickListener{
 
