c8e0783465aa6619c3c84402f704d757de93c857
Amanda Eliasson <amanda.eliasson@gmail.com>
2016-05-03 23:22:09 +0200

    Implemented accelerometer driven lock mechanism in LockFragment

--- app/src/main/java/com/axelfriberg/bikebuddy/LockFragment.java
+++ app/src/main/java/com/axelfriberg/bikebuddy/LockFragment.java
@@ -2,8 +2,13 @@ package com.axelfriberg.bikebuddy;
 
 
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +20,13 @@ import android.widget.TextView;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LockFragment extends Fragment implements View.OnClickListener {
+public class LockFragment extends Fragment implements View.OnClickListener, SensorEventListener {
     private Button b;
     private TextView locked;
     private ImageView lockImage;
+    Sensor accelerometer;
+    SensorManager sm;
+    boolean yes;
 
     public LockFragment() {
 
@@ -27,17 +35,21 @@ public class LockFragment extends Fragment implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.navigation_item_lock);
+        sm = (SensorManager)getActivity().getSystemService(getActivity().SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View v = inflater.inflate(R.layout.fragment_lock, container, false);
         b = (Button)v.findViewById(R.id.button_lock);
         locked = (TextView)v.findViewById(R.id.locked_text);
         lockImage = (ImageView)v.findViewById(R.id.lock_image);
         b.setOnClickListener(this);
+
         return v;
     }
 
@@ -54,9 +66,36 @@ public class LockFragment extends Fragment implements View.OnClickListener {
         }
 
     }
+
     public void onResume(){
         super.onResume();
         getActivity().setTitle(R.string.navigation_item_lock);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+            float z = event.values[2];
+
+            if(z < -8 && yes == false){
+                if (b.getText().equals("Unlock")) {
+                    b.setText("Lock");
+                    locked.setText("Your bike is unlocked");
+                    lockImage.setImageResource(R.drawable.unlock_lock);
+                }else{
+                    b.setText("Unlock");
+                    locked.setText("Your bike is locked");
+                    lockImage.setImageResource(R.drawable.lock_lock);
+                }
+                yes = true;
+            }
+            if(z > 8 && yes == true){
+                yes = false;
+            }
+        }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
 
